cfcfb487576ad8760069bda30c8426f25e622246
Dmitry Grechko <dmitry.grechko@deskree.com>
2020-06-08 20:24:28 -0400

    Initiated xCode project
    Instantiated Swift project using SwiftUI as a single view app

---
+++ Fisk Book/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Fisk Book
+//
+//  Created by Grechko Dmytro on 2020-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
